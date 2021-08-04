31510da31aa3da00ef4e582f98754c6091a479c0
Mikhail <mikhailgrigoriev92@gmail.com>
2021-08-03 23:34:48 -0400

    Create skeleton files

---
+++ README.md
@@ -0,0 +1,17 @@
+Calculator Project
+
+A calculator capable of performing addition, subtraction, multiplication, and division functions.
+
+Demo
+
+-Include picture of finished calculator. 
+
+Features
+
+How to use
+
+Possible future additions
+
+Known Bugs
+
+Reflections
---
+++ javascript.js
@@ -0,0 +1,20 @@
+function addition(firstNumber, secondNumber){
+    return firstNumber + secondNumber;
+};
+
+function subtraction(firstNumber, secondNumber){
+    return firstNumber - secondNumber;
+};
+
+function multiplication(firstNumber, secondNumber){
+    return firstNumber * secondNumber;
+};
+
+function division(firstNumber, secondNumber){
+    return firstNumber / secondNumber;
+};
+
+console.log(addition(6,3));
+console.log(subtraction(6,3));
+console.log(multiplication(6,3));
+console.log(division(6,3));

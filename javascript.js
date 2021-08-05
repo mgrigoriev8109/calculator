810d88df930bc73b35ace5f90a9610c9135951a5
Mikhail <mikhailgrigoriev92@gmail.com>
2021-08-04 23:16:26 -0400

    Begin calculate() function

--- javascript.js
+++ javascript.js
@@ -1,12 +1,26 @@
-function addEventListenersToButtons(){
-    const calculatorButtonsArray = document.getElementsByClassName("calculatorButton");
-    for (let buttonInArray = 0; buttonInArray < calculatorButtonsArray.length; buttonInArray++){
-        calculatorButtonsArray[buttonInArray].addEventListener("click", () => {
-            
-            populateDisplay(calculatorButtonsArray[buttonInArray].textContent);
-        });
-    };
+// let's think about what we're trying to code. we have displayArray storing our values
+// there needs to be a conditional at some point watching over the values going into 
+// the display Array. Possibly through a nested conditional watch to see when numbers
+// become an operator, then numbers again, then an operator again. This second time 
+// they become an operator, all the numbers before the operator are concatenated into 
+// a single number, all those after are concatenated, and we perform operate(). If there
+// is another operator this continues, if not this is the final value
+
+function calculate(){
+    let calculatedValue = displayArray.reduce((accumulator, currentValue) => {
+
+    }, 0);
+}
+
+
+const calculatorButtonsArray = document.getElementsByClassName("calculatorButton");
+for (let buttonInArray = 0; buttonInArray < calculatorButtonsArray.length; buttonInArray++){
+    calculatorButtonsArray[buttonInArray].addEventListener("click", () => {            
+        populateDisplay(calculatorButtonsArray[buttonInArray].textContent);
+        console.log(displayArray);
+    });
 };
+
 
 
 function addition(firstNumber, secondNumber){
@@ -43,11 +57,9 @@ function operate(operator, firstNumber, secondNumber){
 };
 
 function populateDisplay(calculatorButtonClicked){
-    // function that populates the display when clicking buttons
-    // store the display value in a variable for the next step
     const displayValue = document.getElementById("displayValue");
     displayValue.value += calculatorButtonClicked;
-    let displayArray = displayValue.value.split("");
+    return displayArray = displayValue.value.split("");
 };
 
 addEventListenersToButtons();
